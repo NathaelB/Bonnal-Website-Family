830804980101af86985a791df5012270e5cec276
Nathael Bonnal <kirua48000@gmail.com>
2022-04-09 23:53:56 +0200

    :construction: create group for manager & auth

--- start/routes.ts
+++ start/routes.ts
@@ -17,9 +17,35 @@
 | import './routes/customer''
 |
 */
-
+const links = [
+  {name: "Accueil", path: 'home'},
+  {name: "Blog", path: 'blog'}
+]
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/', async ({ view }) => {
-  return view.render('pages/index')
+  return view.render('pages/index', links)
 }).as('home')
+
+
+Route.get('/blog', async ({view}) => {
+  return view.render('pages/blog/index', links)
+}).as('blog')
+
+
+Route.group(() => {
+  Route.get('/login', 'AuthController.login').as('login')
+  Route.get('/register', 'AuthController.register').as('register')
+
+  Route.post('/login', 'AuthController.loginWeb')
+  Route.post('/register', 'AuthController.registerWeb')
+  Route.post('/logout', 'AuthController.logoutWeb')
+}).prefix('authentication')
+
+Route.group(() => {
+  Route.get('/', 'ManagersController.index').as('manager.home')
+
+  Route.group(() => {
+    Route.get('/', 'UsersController.index').as('manager.users')
+  }).prefix('users')
+}).prefix('manager')
