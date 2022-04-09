751cb6633562532402ef9b3be9bcb9afd414df6b
Nathael Bonnal <kirua48000@gmail.com>
2022-04-09 23:51:52 +0200

    :white_check_mark: add `dropdown` data

--- resources/ts/app.ts
+++ resources/ts/app.ts
@@ -1,1 +1,13 @@
 import '../css/app.css'
+import '../css/carroussel.scss'
+import Alpine from 'alpinejs'
+
+Alpine.data('dropdown', () => ({
+  open: false,
+
+  toggle() {
+    this.open = !this.open
+  }
+}))
+
+Alpine.start()
